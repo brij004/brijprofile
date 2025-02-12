dbaed16a6ec99f836271d002ffa489881b8bf8a9
brij_r_24 <brijp6386@gmail.com>
2025-02-12 12:07:33 +0530

    resume design update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Brij_Ratanpara-cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/entry.webpack"; // Required for Next.js or Webpack
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -17,30 +17,19 @@ function ResumeNew() {
 
   return (
     <div>
-      <Container fluid className="resume-section">
+      <Container
+        fluid
+        className="pt-32 pb-7"
+        style={{
+          background:
+            "linear-gradient(to bottom left, rgba(17, 16, 16, 0.582), rgba(12, 8, 24, 0.904))",
+        }}
+      >
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
+            href="/Brij_Ratanpara-cv.pdf"
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -49,6 +38,61 @@ function ResumeNew() {
           </Button>
         </Row>
       </Container>
+      {/* pdf */}
+      <div
+        className="flex flex-col items-center justify-center min-h-screen pb-4 pt-4 text-white relative"
+        style={{
+          background:
+            "linear-gradient(to bottom left, rgba(17, 16, 16, 0.582), rgba(12, 8, 24, 0.904))",
+        }}
+      >
+        <Document
+          file="/Brij_Ratanpara-cv.pdf" // Your PDF file inside the "public" folder
+          onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+          className="flex justify-center"
+        >
+          <Page
+            pageNumber={pageNumber}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
+        </Document>
+
+        {/* Pagination Controls  */}
+        <div className="flex gap-5 mt-4">
+          <button
+            disabled={pageNumber <= 1}
+            onClick={() => setPageNumber(pageNumber - 1)}
+            className={`px-4 py-2 rounded ${
+              pageNumber <= 1
+                ? "bg-[#6464649a] cursor-not-allowed"
+                : "bg-[#623686] hover:bg-[#552e72ec]"
+            }`}
+          >
+            Previous
+          </button>
+
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+
+          <button
+            disabled={pageNumber >= numPages}
+            onClick={() => setPageNumber(pageNumber + 1)}
+            className={`px-4 py-2 rounded ${
+              pageNumber >= numPages
+                ? "bg-[#6464649a] cursor-not-allowed"
+                : "bg-[#623686] hover:bg-[#552e72ec]"
+            }`}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+
+      {/* pdf puri */}
+
+      <Particle />
     </div>
   );
 }
